4c6dea3063fcca980029815e4b971df6298f97b3
adirb1 <58398426+adirb1@users.noreply.github.com>
2021-10-06 19:17:37 +0300

    deploy

--- messages/settings.py
+++ messages/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,7 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 
